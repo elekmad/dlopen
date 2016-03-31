9f934d66e98db7e86c0adb5d2fd79ba07d243c4f
elekmad <damien.warin@gmail.com>
2016-03-31 16:31:25 +0200

    spécification de la lib dl a charger via argv1.

--- main.c
+++ main.c
@@ -6,15 +6,23 @@
 
 int main(int argc, char **argv)
 {
-    void *dlhandle = dlopen("libDLLib1.so", RTLD_LAZY);
-    if(dlhandle == NULL)
-        printf("dlopen error : %s\n", dlerror());
-    char *(*dlop1)(void) = (char *(*)(void))dlsym(dlhandle, "dlop1");
-    if(dlop1 == NULL)
-        printf("dlopen error : %s\n", dlerror());
-    else
-        printf ("Result DL  = %s\n", dlop1());
+    void *dlhandle = NULL;
+    if(argc > 1)
+    {
+        char *(*dlop1)(void) = NULL;
+        dlhandle = dlopen(argv[1], RTLD_LAZY);
+        if(dlhandle == NULL)
+            printf("dlopen error : %s\n", dlerror());
+        else
+        {
+            dlop1 = (char *(*)(void))dlsym(dlhandle, "dlop1");
+            if(dlop1 == NULL)
+                printf("dlopen error : %s\n", dlerror());
+            else
+                printf ("Result DL  = %s\n", dlop1());
+            dlclose(dlhandle);
+        }
+    }
     printf ("Result Shared = %s\n", op1());
-    dlclose(dlhandle);
     return EXIT_SUCCESS;
 }
